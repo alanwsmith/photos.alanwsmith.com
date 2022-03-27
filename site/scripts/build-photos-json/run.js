fecc4d2bb40f1eacc61c4da1e92904db80b405d8
Alan Smith <commit@alanwsmith.com>
2022-03-26 23:04:29 -0400

    Making a json file with the details for the photos

--- site/scripts/build-photos-json/run.js
+++ site/scripts/build-photos-json/run.js
@@ -1,8 +1,24 @@
 const { listDir } = require('./listDir')
 const configs = require('./configs')
+const fs = require('fs')
 
 const config = configs.dev
 
-console.log('hello world')
+const file_list = listDir(config.local_source_root)
 
-console.dir(config)
+const json_data = {
+  photos: [],
+}
+
+for (const file of file_list) {
+  console.log(file)
+  try {
+    const data = fs.readFileSync(file.full_path, 'utf8')
+    json_data.photos.push(JSON.parse(data))
+    console.log(data)
+  } catch (err) {}
+}
+
+console.log(json_data)
+
+fs.writeFileSync(config.json_storage_path, JSON.stringify(json_data))
